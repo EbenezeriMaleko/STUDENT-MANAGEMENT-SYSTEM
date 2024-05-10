bff20e24ada71e970f1cac09a2422ca06b197d2e
john petro <johnpetro335@gmail.com>
2024-05-10 17:33:26 +0300

    frist commit

---
+++ SOMA/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using SOMA.Models;
+
+namespace SOMA.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+    public IActionResult Apply()
+    {
+          return View();
+    }
+    public IActionResult Login()
+    {
+          return View();
+    }
+
+    // public IActionResult Privacy()
+    // {
+    //     return View();
+    // }
+
+    // public IActionResult Privacy()
+    // {
+    //     return View();
+    // }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
